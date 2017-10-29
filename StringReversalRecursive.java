a6820365f1379732f7fdb653e470514718da45b2
bobbymcgonigle <mcgonigb@tcd.ie>
2017-10-29 17:41:19 +0000

    Added recursive string reversal question

---
+++ StringReversalRecursive.java
@@ -0,0 +1,23 @@
+
+public class StringReversalRecursive {
+
+	String reversedString = "";
+		
+	public String reverse(String str)
+	{
+		if(str.length() == 1){
+            return str;
+        } 
+		else
+        {
+            reversedString += str.charAt(str.length()-1) +reverse(str.substring(0,str.length()-1));
+            return reversedString;
+        }
+	}
+	
+	public static void main(String[] args) {
+		StringReversalRecursive srr = new StringReversalRecursive();
+        System.out.println(srr.reverse("Whatever String You Want"));
+	}
+
+}
