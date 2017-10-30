5be4436aec5fee8819b26fabd3aacb1216deaee1
bobbymcgonigle <mcgonigb@tcd.ie>
2017-10-30 14:23:49 +0000

    format answers

--- ArrayList.java
+++ ArrayList.java
@@ -1,57 +1,66 @@
 /*Write a program to implement your own ArrayList class. It should
 contain add(), get(), remove(), size() methods.
-*/ 
+*/
 
-public class ArrayList<Item> {
+public class ArrayList<T> {
 	private Object[] storage;
 	private int currentSize;
-	
-	public ArrayList()
-	{
+
+	public ArrayList() {
 		storage = new Object[10];
 	}
-	
-	public int size()
-	{
+
+	public int size() {
 		return currentSize;
 	}
-
-	public Object remove(int index){
+	
+	public Object get(int index)
+	{
+		//only return if the index is actually in the bounds of array
 		if(index < currentSize)
-        {
-        	Object obj = storage[index];
-        storage[index] = null;
-        int tmp = index;
-        while(tmp < currentSize)
-        {
-        	storage[tmp] = storage[tmp+1];
-        	storage[tmp+1] = null;
-        	tmp++;
-        }
-        currentSize--;
-        return obj; 
-        }
-		else {
-            throw new ArrayIndexOutOfBoundsException();
-        }
+		{
+			return storage[index];
+		}
+		else
+		{
+			throw new ArrayIndexOutOfBoundsException();
+		}
 	}
 	
-	private void resize(int max)
-	{ // Move stack to a new array of size max.
+	public Object remove(int index)
+	{
+		if (index < currentSize) {
+			Object obj = storage[index];
+			storage[index] = null;
+			int tmp = index;
+			
+			while (tmp < currentSize) {
+				storage[tmp] = storage[tmp + 1];
+				storage[tmp + 1] = null;
+				tmp++;
+			}
+			currentSize--;
+			return obj;
+		} else {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+	}
+
+	private void resize(int max) { // Move stack to a new array of size max.
 		Object[] temp = new Object[max];
-		for (int i = 0; i <currentSize ; i++)
+		for (int i = 0; i < currentSize; i++) 
 		{
 			temp[i] = storage[i];
 		}
 		storage = temp;
 	}
-	
-	public void add(Object obj){
-		//if the array is full, double it in size before adding object
-        if(currentSize == storage.length){
-            resize(2*storage.length);
-        }
-        storage[currentSize] = obj;
-    }
-	
+
+	public void add(Object obj) {
+		// if the array is full, double it in size before adding object
+		if (currentSize == storage.length) {
+			resize(2 * storage.length);
+		}
+		storage[currentSize++] = obj;
+	}
+
 }
--- StringReversalRecursive.java
+++ StringReversalRecursive.java
@@ -1,25 +1,20 @@
-//Write a program to reverse a string using recursive methods.
 
 public class StringReversalRecursive {
 
 	String reversedString = "";
-		
-	public String reverse(String str)
-	{
-		if(str.length() == 1){
-            return str;
-        } 
-		else
-        {
-			//return string = return string + character at length-1 + call(reverse of index 0-length-1
-            reversedString += str.charAt(str.length()-1) +reverse(str.substring(0,str.length()-1));
-            return reversedString;
-        }
+
+	public String reverse(String str) {
+		if (str.length() == 1) {
+			return str;
+		} else {
+			reversedString += str.charAt(str.length() - 1) + reverse(str.substring(0, str.length() - 1));
+			return reversedString;
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		StringReversalRecursive srr = new StringReversalRecursive();
-        System.out.println(srr.reverse("Whatever String You Want"));
+		System.out.println(srr.reverse("Whatever String You Want"));
 	}
 
 }
