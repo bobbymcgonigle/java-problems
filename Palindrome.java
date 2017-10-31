23748a6611a02db890d6d2a064d6579ea80c91b2
Bobby McGonigle <mcgonigb@tcd.ie>
2017-10-31 12:59:57 +0000

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,19 @@
+
+public class Palindrome {
+		public static boolean isPalindrom(char[] word)
+		{
+			//if String passed in just use toCharArray() function
+		    int forwardsIndex = 0;
+		    int backwardsIndex = word.length - 1;
+		    while (backwardsIndex > forwardsIndex) 
+		    {
+		        if (word[forwardsIndex] != word[backwardsIndex]) 
+		        {
+		            return false;
+		        }
+		        forwardsIndex++;
+		        backwardsIndex--;
+		    }
+		    return true;
+		}
+}
