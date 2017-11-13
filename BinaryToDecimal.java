7f8233baef9f7f7aa9ece3354927e66a7037d9d5
Bobby McGonigle <mcgonigb@tcd.ie>
2017-11-13 19:27:48 +0000

    Binary to decimal problem

---
+++ BinaryToDecimal.java
@@ -0,0 +1,22 @@
+public class BinaryToDecimal {
+ 
+    public static int getDecimalFromBinary(int binary){
+         
+        int decimal = 0;
+        int power = 0;
+        while(true){
+            if(binary == 0){
+                break;
+            } else {
+                int tmp = binary%10;
+                decimal += tmp*Math.pow(2, power);
+                binary = binary/10;
+                power++;
+            }
+        }
+        return decimal;
+    }
+     
+    public static void main(String a[]){
+    }
+}
